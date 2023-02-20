39cf132d7133533773cf30f60f32759ef74ba5b3
MouadFiali <fialimouad@gmail.com>
2023-02-20 19:56:44 +0100

    first commit

---
+++ SchoolMateAPI/src/main/java/com/manager/schoolmateapi/SchoolMateApiApplication.java
@@ -0,0 +1,13 @@
+package com.manager.schoolmateapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolMateApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolMateApiApplication.class, args);
+	}
+
+}
