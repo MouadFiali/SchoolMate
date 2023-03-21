a32bdf85835c9b9c1b8d6ead65b5781f2076e6e7
zakaria.68K <zakaria.hachm20@gmail.com>
2023-03-21 16:07:50 +0000

    feat: adding the alert type

---
+++ src/main/java/com/manager/schoolmateapi/alerts/Alert.java
@@ -0,0 +1,49 @@
+package com.manager.schoolmateapi.alerts;
+
+import org.springframework.data.geo.Point;
+
+import com.manager.schoolmateapi.alerts.enumerations.AlertType;
+import com.manager.schoolmateapi.users.models.User;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "alerts")
+public class Alert {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name ="user_id")
+    private User user;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.ORDINAL)
+    private AlertType type;
+
+    @Column(nullable = false)
+    private Point coordinates;
+}
