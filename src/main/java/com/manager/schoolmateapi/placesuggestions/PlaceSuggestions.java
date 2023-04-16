7fcc25c8ca9ce1ee92687edb47c5888d79403238
Mouad EL OUARTI <mouadelouarti1@gmail.com>
2023-04-16 13:46:27 +0000

    Adding the PlaceSuggestions model layer

---
+++ src/main/java/com/manager/schoolmateapi/placesuggestions/PlaceSuggestions.java
@@ -0,0 +1,46 @@
+package com.manager.schoolmateapi.placesuggestions;
+
+import org.springframework.data.geo.Point;
+import com.manager.schoolmateapi.placesuggestions.enumerations.PlaceSuggestionType;
+import com.manager.schoolmateapi.users.models.User;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "placesuggestions")
+public class PlaceSuggestions {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne( fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.ORDINAL)
+    private PlaceSuggestionType suggestiontype;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private Point location;
+
+}
