f38bbef3aba2ed5372c1d6d1736893c6ad111392
Mouad EL OUARTI <mouadelouarti1@gmail.com>
2023-04-16 13:55:56 +0000

    Adding the PlaceSuggestionsRepository

---
+++ src/main/java/com/manager/schoolmateapi/placesuggestions/PlaceSuggestionRepository.java
@@ -0,0 +1,7 @@
+package com.manager.schoolmateapi.placesuggestions;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PlaceSuggestionRepository extends JpaRepository< PlaceSuggestions,Long >{
+    
+}
