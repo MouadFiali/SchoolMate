ecee25921054726c0561b295c44cc772f4fabb13
MouadFiali <fialimouad@gmail.com>
2023-04-19 15:58:18 +0000

    fix: adding needed annotations to the complaint subclasses

--- src/main/java/com/manager/schoolmateapi/complaints/models/BuildingComplaint.java
+++ src/main/java/com/manager/schoolmateapi/complaints/models/BuildingComplaint.java
@@ -4,7 +4,17 @@ import com.manager.schoolmateapi.complaints.enumerations.BuildingProb;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+@EqualsAndHashCode(callSuper=false)
 @Entity
 public class BuildingComplaint extends Complaint {
     
--- src/main/java/com/manager/schoolmateapi/complaints/models/FacilitiesComplaint.java
+++ src/main/java/com/manager/schoolmateapi/complaints/models/FacilitiesComplaint.java
@@ -4,7 +4,17 @@ import com.manager.schoolmateapi.complaints.enumerations.FacilityType;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+@EqualsAndHashCode(callSuper=false)
 @Entity
 public class FacilitiesComplaint extends Complaint {
 
@@ -13,4 +23,5 @@ public class FacilitiesComplaint extends Complaint {
 
     @Column(name = "class", nullable = false)
     private String className;
+    
 }
