6ca7aece81c79948b13f41300b9c94bfde514cd3
MouadFiali <fialimouad@gmail.com>
2023-04-26 02:06:50 +0100

    fix: adding necessary annotations for the superclass dto

--- src/main/java/com/manager/schoolmateapi/complaints/dto/CreateComplaintDto.java
+++ src/main/java/com/manager/schoolmateapi/complaints/dto/CreateComplaintDto.java
@@ -1,5 +1,8 @@
 package com.manager.schoolmateapi.complaints.dto;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 import lombok.AllArgsConstructor;
@@ -10,6 +13,12 @@ import lombok.experimental.SuperBuilder;
 @AllArgsConstructor
 @NoArgsConstructor
 @SuperBuilder
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY)
+@JsonSubTypes({
+        @JsonSubTypes.Type(value = CreateBuildingComplaintDto.class, name = "building"),
+        @JsonSubTypes.Type(value = CreateRoomComplaintDto.class, name = "room"),
+        @JsonSubTypes.Type(value = CreateFacilityComplaintDto.class, name = "facility")
+})
 public class CreateComplaintDto {
     
     @NotBlank(message = "The description is required")
