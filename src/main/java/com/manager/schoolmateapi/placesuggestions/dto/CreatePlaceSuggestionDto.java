0152a72dfd12621d550a70390648e671ccdb50b6
Mouad EL OUARTI <mouadelouarti1@gmail.com>
2023-04-30 19:11:54 +0100

    fix: Changing coordinates types & adding builder

--- src/main/java/com/manager/schoolmateapi/placesuggestions/dto/CreatePlaceSuggestionDto.java
+++ src/main/java/com/manager/schoolmateapi/placesuggestions/dto/CreatePlaceSuggestionDto.java
@@ -1,22 +1,28 @@
 package com.manager.schoolmateapi.placesuggestions.dto;
 
+import java.util.List;
+
 import com.manager.schoolmateapi.placesuggestions.enumerations.PlaceSuggestionType;
+
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
 @Data
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
 public class CreatePlaceSuggestionDto {
 
     @NotBlank(message = "A description is required")
     private String description;
 
     @NotNull(message = "The category is necessary")
-    private PlaceSuggestionType type;
+    private PlaceSuggestionType suggestiontype;
 
-    @NotNull
-    private Double latitude;
-
-    @NotNull
-    private Double longitude;
+    @NotNull(message = "The coordinates are required")
+    private List<Double> coordinates;
 }
--- src/main/java/com/manager/schoolmateapi/placesuggestions/dto/EditPlaceSuggestionDto.java
+++ src/main/java/com/manager/schoolmateapi/placesuggestions/dto/EditPlaceSuggestionDto.java
@@ -1,21 +1,26 @@
 package com.manager.schoolmateapi.placesuggestions.dto;
 
+import org.springframework.data.geo.Point;
+
 import com.manager.schoolmateapi.placesuggestions.enumerations.PlaceSuggestionType;
 
 import io.micrometer.common.lang.Nullable;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class EditPlaceSuggestionDto {
     @Nullable
     private String  description;
 
     @Nullable
-    private PlaceSuggestionType type;
+    private PlaceSuggestionType suggestiontype;
 
     @Nullable
-    private Double latitude;
-
-    @Nullable
-    private Double longitude;
+    private Point coordinates;
 }
