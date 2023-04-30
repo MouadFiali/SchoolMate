0c049243bfcbb47b53f31a94e87f64ed2973c190
Mouad EL OUARTI <mouadelouarti1@gmail.com>
2023-04-30 19:12:48 +0100

    fix: Adding methods to PlaceSuggestionRepository

--- src/main/java/com/manager/schoolmateapi/placesuggestions/PlaceSuggestionRepository.java
+++ src/main/java/com/manager/schoolmateapi/placesuggestions/PlaceSuggestionRepository.java
@@ -1,7 +1,15 @@
 package com.manager.schoolmateapi.placesuggestions;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PlaceSuggestionRepository extends JpaRepository< PlaceSuggestions,Long >{
-    
+import com.manager.schoolmateapi.users.models.User;
+
+public interface PlaceSuggestionRepository extends JpaRepository<PlaceSuggestions, Long> {
+
+    public Optional<PlaceSuggestions> findByIdAndUser(long id, User user);
+
+    public Iterable<PlaceSuggestions> findByUser(User user);
+
 }
